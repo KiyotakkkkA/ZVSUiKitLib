6ad2b2925b04bb9125fda9456f121adfebf42ce0
KiyotakkkkA <kirzaha2@mail.ru>
2026-08-21 13:20:55 +0300

    feat(loader): add label prop for accessibility and localization

--- package/src/ui/Loader/types.ts
+++ package/src/ui/Loader/types.ts
@@ -4,6 +4,8 @@ import type { DivClassName } from "../_shared/types";
 export type LoaderProps = {
     /** Receives the loader element. */
     ref?: Ref<HTMLDivElement>;
+    /** Loading label */
+    label?: string;
     /** Applies CSS classes to the spinner SVG. */
     className?: DivClassName;
 };
